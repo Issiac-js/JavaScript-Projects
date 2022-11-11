20a5cbb1e3bdf4bee24fc14144f1ad648c882045
Issiac-js <115911939+Issiac-js@users.noreply.github.com>
2022-11-11 14:28:29 -0800

    Added New JavaScript

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,23 @@
+window.alert("Hello, Wolrd!"); //makes a pop-up saying "Hellow, world!"
+document.write('Hello, World!'); // writes on the webpage the string
+var B = "<br> This is a string"; // a varible with a string
+document.write(B); // writes string to  webpage
+
+var aQuote = "<br>Sam said, \"You need to catch up\""; // another varible
+document.write(aQuote); // writes the string stored in var to webpage
+
+document.write("<br>I like cars." + " That move fast!<br>"); // concatenate and writes to webpage
+
+var aString = "Concatenated" + " string<br>"; // concatenate but this time stored in a var
+
+document.write(aString) // prints stored concatenated string to webpage
+
+var Family = "Sasse", Dad = "Jonathan", Mom = "Melissa", Sister = "Kayla"; // a list of vars with strings for data types
+document.write(Mom); // prints to webpage string stored in var mom.
+document.write(3 + 3); // does math operation and prints it to webpage.
+
+var sent1 = "<br>This is the beginning of the string"; // first part of the final string 
+
+var sent2 = " and this is the end of the string"; // second part of the final string
+
+document.write(sent1 + sent2); // prints concatenated string to webpage.
